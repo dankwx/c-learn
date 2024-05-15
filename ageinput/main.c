ede8149d4d8532f32ea103296558bafbcb587629
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-05-15 14:41:53 -0300

    featuer: add projects

---
+++ ageinput/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int idade;
+    printf("-------------\n");
+    printf("Insira sua idade: ");
+    scanf("%d", &idade);
+    printf("Sua idade e: %d anos.\n", idade);
+    printf("-------------\n");
+    return 0;
+}
