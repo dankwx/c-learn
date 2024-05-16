47cee9ec58b92f37713b1e45247d52bb77fddae8
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-05-15 21:57:12 -0300

    feature:add validation if exists a claimed position

--- tictactoe/main.c
+++ tictactoe/main.c
@@ -43,8 +43,11 @@ int main()
     int opcaoPosicao;
     // 0 4 8
     char linhaTopo[] = {'y', ' ', '|', ' ', 'y', ' ', '|', ' ', 'y', '\0'};
+    char linhaTopoRef[] = {'y', ' ', '|', ' ', 'y', ' ', '|', ' ', 'y', '\0'};
     char linhaMeio[] = {'y', ' ', '|', ' ', 'y', ' ', '|', ' ', 'y', '\0'};
+    char linhaMeioRef[] = {'y', ' ', '|', ' ', 'y', ' ', '|', ' ', 'y', '\0'};
     char linhaBaixo[] = {'y', ' ', '|', ' ', 'y', ' ', '|', ' ', 'y', '\0'};
+    char linhaBaixoRef[] = {'y', ' ', '|', ' ', 'y', ' ', '|', ' ', 'y', '\0'};
 
     while (1)
     {
@@ -57,12 +60,34 @@ int main()
         switch (opcaoLinha)
         {
         case 1:
-            linhaTopo[opcaoPosicao] = 'x';
+            if (linhaTopo[opcaoPosicao] == linhaTopoRef[opcaoPosicao])
+            {
+                linhaTopo[opcaoPosicao] = 'x';
+                break;
+            }
+
+            printf("nao daaaaa");
             break;
         case 2:
+            if (linhaMeio[opcaoPosicao] == linhaMeioRef[opcaoPosicao])
+            {
+                linhaMeio[opcaoPosicao] = 'x';
+                break;
+            }
+            printf("nao daaaaa");
+            break;
+
             linhaMeio[opcaoPosicao] = 'x';
             break;
         case 3:
+            if (linhaBaixo[opcaoPosicao] == linhaBaixoRef[opcaoPosicao])
+            {
+                linhaBaixo[opcaoPosicao] = 'x';
+                break;
+            }
+            printf("nao daaaaa");
+            break;
+
             linhaBaixo[opcaoPosicao] = 'x';
             break;
 
