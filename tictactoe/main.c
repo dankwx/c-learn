db552e6dd200619c40a689c0b8345085924a32ba
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-05-16 19:03:06 -0300

    feature: add ia victory validation

--- tictactoe/main.c
+++ tictactoe/main.c
@@ -80,6 +80,57 @@ int todasPosicoesDiferentesBaixo(char linhaBaixo[], char linhaBaixoRef[], int ta
     return 1; // Todas as posições com 'y' em linhaTopoRef são diferentes de 'y' em linhaTopo
 };
 
+// verificar vitoria jogador
+int verificarVitoriaJogador(char linhaTopo[], char linhaMeio[], char linhaBaixo[], int tamanho)
+{
+    int i = 0;
+    while (i < tamanho)
+    {
+        // verifica vitorias verticais
+        if (linhaTopo[i] == 'x' && linhaMeio[i] == 'x' && linhaBaixo[i] == 'x')
+        {
+            return 1;
+        }
+        // verifica vitorias horizontal
+        else if ((linhaTopo[0] == 'x' && linhaTopo[4] == 'x' && linhaTopo[8] == 'x') || (linhaMeio[0] == 'x' && linhaMeio[4] == 'x' && linhaMeio[8] == 'x') || (linhaBaixo[0] == 'x' && linhaBaixo[4] == 'x' && linhaBaixo[8] == 'x'))
+        {
+            return 1;
+        }
+        else if ((linhaTopo[0] == 'x' && linhaMeio[4] == 'x' && linhaBaixo[8] == 'x') || (linhaBaixo[0] == 'x' && linhaMeio[4] == 'x' && linhaTopo[8] == 'x'))
+        {
+            return 1;
+        }
+
+        i++;
+    };
+    return 0;
+};
+// verificar vitoria IA
+int verificarVitoriaIa(char linhaTopo[], char linhaMeio[], char linhaBaixo[], int tamanho)
+{
+    int i = 0;
+    while (i < tamanho)
+    {
+        // verifica vitorias verticais
+        if (linhaTopo[i] == 'o' && linhaMeio[i] == 'o' && linhaBaixo[i] == 'o')
+        {
+            return 1;
+        }
+        // verifica vitorias horizontal
+        else if ((linhaTopo[0] == 'o' && linhaTopo[4] == 'o' && linhaTopo[8] == 'o') || (linhaMeio[0] == 'o' && linhaMeio[4] == 'o' && linhaMeio[8] == 'o') || (linhaBaixo[0] == 'o' && linhaBaixo[4] == 'o' && linhaBaixo[8] == 'o'))
+        {
+            return 1;
+        }
+        else if ((linhaTopo[0] == 'o' && linhaMeio[4] == 'o' && linhaBaixo[8] == 'o') || (linhaBaixo[0] == 'o' && linhaMeio[4] == 'o' && linhaTopo[8] == 'o'))
+        {
+            return 1;
+        }
+
+        i++;
+    };
+    return 0;
+};
+
 int main()
 {
     int opcaoLinha;
@@ -101,10 +152,34 @@ int main()
     while (1)
     {
 
-        // verificacao empate
         int tamanho = sizeof(linhaTopo) / sizeof(linhaTopo[0]) - 1;
         int tamanhoMeio = sizeof(linhaMeio) / sizeof(linhaMeio[0]) - 1;
-        int tamanhoBaixo = sizeof(linhaBaixo) / sizeof(linhaBaixo[0]) - 1; // Subtrai 1 para desconsiderar o '\0'
+        int tamanhoBaixo = sizeof(linhaBaixo) / sizeof(linhaBaixo[0]) - 1;
+        // verificar vitoria
+        if (verificarVitoriaJogador(linhaTopo, linhaMeio, linhaBaixo, tamanho))
+        {
+            printf("%s\n", linhaTopo);
+            printf("%s\n", divisoria);
+            printf("%s\n", linhaMeio);
+            printf("%s\n", divisoria);
+            printf("%s\n\n", linhaBaixo);
+            printf("\nVoce ganhou.\n");
+            jogoFinalizado = 1;
+            break;
+        }
+        else if (verificarVitoriaIa(linhaTopo, linhaMeio, linhaBaixo, tamanho))
+        {
+            printf("%s\n", linhaTopo);
+            printf("%s\n", divisoria);
+            printf("%s\n", linhaMeio);
+            printf("%s\n", divisoria);
+            printf("%s\n\n", linhaBaixo);
+            printf("\nVoce perdeu.\n");
+            jogoFinalizado = 1;
+            break;
+        }
+
+        // verificacao empate
         if (todasPosicoesDiferentesTopo(linhaTopo, linhaTopoRef, tamanho) && todasPosicoesDiferentesMeio(linhaMeio, linhaMeioRef, tamanhoMeio) && todasPosicoesDiferentesBaixo(linhaBaixo, linhaBaixoRef, tamanhoBaixo))
         {
             printf("O jogo empatou.\n");
@@ -132,7 +207,7 @@ int main()
                     break;
                 }
 
-                printf("nao daaaaa");
+                // printf("nao daaaaa");
                 break;
             case 2:
                 if (linhaMeio[opcaoPosicao] == linhaMeioRef[opcaoPosicao])
@@ -140,7 +215,7 @@ int main()
                     linhaMeio[opcaoPosicao] = 'x';
                     break;
                 }
-                printf("nao daaaaa");
+                // printf("nao daaaaa");
                 break;
 
                 linhaMeio[opcaoPosicao] = 'x';
@@ -151,7 +226,7 @@ int main()
                     linhaBaixo[opcaoPosicao] = 'x';
                     break;
                 }
-                printf("nao daaaaa");
+                // printf("nao daaaaa");
                 break;
 
                 linhaBaixo[opcaoPosicao] = 'x';
